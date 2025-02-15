d59411421b6b2d74205ccf574fd4847a78d65b56
Zhilu Tang <tonythompsoncmu@gmail.com>
2025-02-15 08:34:20 -0800

    Add prod test

--- ViewPager2/testing/src/test/kotlin/androidx/viewpager2/integration/testapp/MinJvmTestTest.kt
+++ ViewPager2/testing/src/test/kotlin/androidx/viewpager2/integration/testapp/MinJvmTestTest.kt
@@ -1,11 +1,17 @@
 package androidx.viewpager2.integration.testapp
 
-import com.google.common.truth.Truth
+import android.content.Context
+import androidx.test.core.app.ApplicationProvider
+import com.google.common.truth.Truth.assertThat
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
 
+@RunWith(RobolectricTestRunner::class)
 class MinJvmTestTest {
 
   @org.junit.Test
   fun test123() {
-    Truth.assertThat(1).isNotEqualTo(2)
+    val context = ApplicationProvider.getApplicationContext<Context>()
+    assertThat(context.packageName).isEqualTo("org.robolectric.default")
   }
 }
