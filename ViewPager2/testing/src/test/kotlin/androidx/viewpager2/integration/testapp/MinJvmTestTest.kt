f656fa6774a204933a755d6b2ac50ee868043ff3
Zhilu Tang <tonythompsoncmu@gmail.com>
2025-02-15 08:43:53 -0800

    Add  PreviewPagesTest test

--- ViewPager2/testing/src/test/kotlin/androidx/viewpager2/integration/testapp/MinJvmTestTest.kt
+++ ViewPager2/testing/src/test/kotlin/androidx/viewpager2/integration/testapp/MinJvmTestTest.kt
@@ -2,16 +2,17 @@ package androidx.viewpager2.integration.testapp
 
 import android.content.Context
 import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.google.common.truth.Truth.assertThat
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
 
-@RunWith(RobolectricTestRunner::class)
+@RunWith(AndroidJUnit4::class)
 class MinJvmTestTest {
 
   @org.junit.Test
   fun test123() {
     val context = ApplicationProvider.getApplicationContext<Context>()
-    assertThat(context.packageName).isEqualTo("org.robolectric.default")
+    assertThat(context.packageName).isEqualTo("androidx.viewpager2.integration.testapp.test.test")
   }
 }
